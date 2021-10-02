6441c617ecdfd5e7b4ebf35775959605eba5c65e
md.rubel <md.rubel@HOAP2406101764A>
2021-10-03 00:20:53 +0600

    Add Student Model in Context and add Seed Data

--- AspNetCoreServerSidePaginationWithDataTables/Data/ApplicationDbContext.cs
+++ AspNetCoreServerSidePaginationWithDataTables/Data/ApplicationDbContext.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+﻿using DatatablesServerSide.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
@@ -11,6 +12,28 @@ namespace AspNetCoreServerSidePaginationWithDataTables.Data
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
             : base(options)
         {
+        }
+
+
+        //StudentList is static to be able to reach in application scope
+        public static List<Student> StudentList { get; set; }
+
+        //Creates “studentCount” students and adds into student list
+        public static void InitStudentList(int studentCount)
+        {
+            StudentList = new List<Student>();
+            for (int i = 1; i < studentCount + 1; i++)
+            {
+                StudentList.Add(
+                    new Student()
+                    {
+                        Id = i,
+                        Firstname = "Firstname" + i,
+                        Lastname = "Lastname" + i,
+                        CreatedDate = DateTime.Now
+                    }
+                );
+            }
         }
     }
 }
